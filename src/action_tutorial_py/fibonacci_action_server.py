85156e62696eb950b36be064a354ecd63f68cfb6
steven-joe-2048 <steven.joe.2048@gmail.com>
2025-04-20 10:57:55 +0800

    ROS2 tutorial from ubuntu 22.04

--- src/action_tutorial_py/fibonacci_action_server.py
+++ src/action_tutorial_py/fibonacci_action_server.py
@@ -1,3 +1,5 @@
+
+import time
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
@@ -17,6 +19,16 @@ class FibonacciActionServer(Node):
 
     def execute_callback(self, goal_handle):
         self.get_logger().info('Executing goal...')
+        # add
+        feedback_msg = Fibonacci.Feedback()
+        feedback_msg.partial_sequence = [0, 1]
+        for i in range(1, goal_handle.request.order):
+            feedback_msg.partial_sequence.append(
+            feedback_msg.partial_sequence[i] + feedback_msg.partial_sequence[i-1])
+            self.get_logger().info('Feedback: {0}'.format(feedback_msg.partial_sequence))
+            goal_handle.publish_feedback(feedback_msg)
+            time.sleep(1)
+        
         result = Fibonacci.Result()
         return result
 
